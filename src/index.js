2b12475fd785e9fce77ce6040634866a78b1baa0
PamelaCB <contrerasbardalesp@gmail.com>
2021-01-17 23:48:16 -0500

    HTML + CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
+ function capturar(){
+    let numeroTarjeta = document.getElementById("txtNum").value;
+    alert(numeroTarjeta);
+} 
+
+capturar();
+
+let numeroTarjeta =  prompt("Ingrese su numero:");
+
 
 console.log(validator);
